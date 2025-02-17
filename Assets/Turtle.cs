c8ff8cee551ef223bc76feb18d626a2ca855bd59
XinweiLyu <lxw491106884@gmail.com>
2025-02-17 07:35:58 -0500

    turtle rotation, forward to enemy, forward in range

--- Assets/Turtle.cs
+++ Assets/Turtle.cs
@@ -7,31 +7,51 @@ public class Turtle : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Transform turtleHeadRotation;
-
+    [SerializeField] private LayerMask enemyMask;
 
     [Header("Attributes")]
-    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float targetingRange = 3f;
+
+    private Transform target;
+
+    private void Update(){
+        if (target == null){
+            FindTarget();
+            return;
+        }
+        RotateTowardsTarget();
+        if (!checkIfTargetInRange()){
+            target = null;
+        }
+    }
+
+    private void FindTarget(){
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)
+        transform.forward, 0f, enemyMask);
+        // 意思是在transform.forward方向上，找到所有在targetingRange范围内的敌人，
+        // (Vector2)transform.forward 是将transform.forward转换为Vector2类型
+        // 0f是忽略所有层, enemyMask是只检测敌人层
+        if (hits.Length > 0){
+            target = hits[0].transform;
+        }
+    }
+
+    private bool checkIfTargetInRange(){
+        return Vector2.Distance(transform.position, target.position) <= targetingRange;
+    }
+
+    private void RotateTowardsTarget(){
+        float angle = Mathf.Atan2(target.position.y - transform.position.y,
+                                    target.position.x - transform.position.x) * Mathf.Rad2Deg-90f;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turtleHeadRotation.rotation = targetRotation;
+    }
 
     private void OnDrawGizmosSelected()
     {
-        Handles.color = Color.cayan;
+        Handles.color = Color.red;
         Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
     }
 
 
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
