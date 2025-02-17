814dc0864126c477baf42a02f87c30e1f7bb7dae
XinweiLyu <lxw491106884@gmail.com>
2025-02-16 23:55:46 -0500

    Changes

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Header("Attributes")]
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
     [SerializeField] private float moveSpeed = 2f;
-        
+
+    private Transform target;
+    private int pathIndex = 0;
+
+    private void Start()
+    {
+        target = LevelManager.main.path[pathIndex];
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f) // 如果敌人到达目标点，就移动到下一个目标点。 <0.1f是为了防止敌人在目标点上抖动
+        {
+            pathIndex++;
+
+            if (pathIndex >= LevelManager.main.path.Length){
+                Destroy(gameObject);
+                return;
+            }else{
+            target = LevelManager.main.path[pathIndex];
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * moveSpeed;
+    }
+
+
 
 }
