f57fa16dfa39431352bbacff429ba1ebf3d64032
XinweiLyu <lxw491106884@gmail.com>
2025-02-16 23:13:17 -0500

    Initial commit - Upload Unity project

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+        
+
+}
