a0497eeef5d2bc9f1b48c8c2813d4db5705cc0f5
XinweiLyu <lxw491106884@gmail.com>
2025-02-17 03:22:29 -0500

    Changes

---
+++ Assets/Code/Script/EnemySpawner.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies= 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f; // 增加这个值，敌人生成速度加快
+    [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+    private bool isSpawning= false;
+
+    private void Start()
+    {
+        StartWave();
+    }
+
+    private void Update()
+    {
+        if (! isSpawning) return;
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && (enemiesLeftToSpawn>0)){
+            SpawnEnemy();
+            enemiesLeftToSpawn--;
+            enemiesAlive++;
+            timeSinceLastSpawn = 0f;
+        }
+    }
+
+    private void StartWave(){
+        isSpawning = true;
+        enemiesLeftToSpawn = EnemiesPerWave();
+    }
+
+    private void SpawnEnemy()
+    {
+        Debug.Log("Spawning enemy");
+        GameObject prefabToSpawn = enemyPrefabs[0];
+        Instantiate(prefabToSpawn, LevelManager.main.startPoint[0].position, Quaternion.identity);
+    }
+
+    private int EnemiesPerWave()
+    {
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave,difficultyScalingFactor));
+    }
+}
